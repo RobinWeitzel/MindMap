3bf59528c1221d9d57c4d5bb939a60e92959ebe6
Robin Weitzel <robin.weitzel@fim-rc.de>
2019-10-07 12:59:29 +0200

    fixed bugs with line breaks

--- script.js
+++ script.js
@@ -50,6 +50,15 @@ const createBubble = (x, y) => {
         save();
     }
 
+    bubble.onkeydown = e => {
+        if (e.keyCode === 13) {
+            // insert 2 br tags (if only one br tag is inserted the cursor won't go to the next line)
+            document.execCommand('insertHTML', false, '<br><br>');
+            // prevent the default behaviour of return key pressed
+            return false;
+        }
+    }
+
     return bubble;
 }
 
